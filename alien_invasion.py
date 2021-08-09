7b5a8b3e14da6fdbca1ac02ce8b80b6d131fee68
Daniil-SyS <khiliuk.ithildan.online>
2021-08-09 15:56:44 +0300

    Назначение цвета фона и создание класса Settings для хранения всех настроек игры

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Класс для управления ресурсами и поведениями игры."""
 
@@ -8,8 +10,13 @@ class AlienInvasion:
         """Инициализирует игру и создает игровые ресурсы"""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        # Назначение цвета фона
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Запуск основного цикла игры"""
@@ -18,6 +25,10 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # При каждом проходе цикла перерисовывается экран
+            self.screen.fill(self.settings.bg_color)
+
             # Отображение последнего прорисованного экрана
             pygame.display.flip()
 
