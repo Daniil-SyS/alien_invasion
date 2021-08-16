26760b978ec825887ea0e766f08f180fb70a17a3
Daniil-SyS <khiliuk.ithildan.online>
2021-08-16 10:26:58 +0300

    Реализован вывод уровня сложности игры и его повышение при уничтожении очередного флота прешельцев

--- alien_invasion.py
+++ alien_invasion.py
@@ -86,6 +86,7 @@ class AlienInvasion:
         self.stats.reset_stats()
         self.stats.game_active = True
         self.sb.prep_score()
+        self.sb.prep_level()
 
         # Очистка списков с прешельцами и снарядами
         self.aliens.empty()
@@ -174,6 +175,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Повышаем уровень
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """
